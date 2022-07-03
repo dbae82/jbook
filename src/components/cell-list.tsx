bee66176ccb98d7b06e6be39988bb97647a44f01
dbae <dbae@thezebra.com>
2022-07-03 11:11:41 -0500

    added add cell component with styles and refactor issue with add cell before action and refactored for after

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,5 +1,7 @@
+import { Fragment } from "react";
 import { useTypedSelector } from "../hooks/use-typed-selector";
 import CellListItem from "./cell-list-item";
+import AddCell from "./add-cell";
 
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) => {
@@ -9,10 +11,18 @@ const CellList: React.FC = () => {
   });
 
   const renderedCells = cells.map((cell) => (
-    <CellListItem key={cell.id} cell={cell} />
+    <Fragment key={cell.id}>
+      <CellListItem cell={cell} />
+      <AddCell prevCellId={cell.id} />
+    </Fragment>
   ));
 
-  return <div>{renderedCells}</div>;
+  return (
+    <div>
+      <AddCell forceVisible={cells.length === 0} prevCellId={null} />
+      {renderedCells}
+    </div>
+  );
 };
 
 export default CellList;
